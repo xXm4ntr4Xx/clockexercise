8ad824b88a3c2d3eba2faa1324347f7031e79485
xXm4ntr4Xx <pietroannobil@gmail.com>
2021-11-24 18:55:25 +0000

    main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+//interaction with the DOM
+const hand = document.querySelector(".hand");
+//create the variable
+
+let clock = 0;
+////function to increase the time by 1
+function time(){
+     hand.style.transform = `rotate(${clock+=6}deg)`;
+	 
+}
+
+
+//// set interval to call the function
+setInterval(()=>time(),1000);
+
+//let movinghand = hand.style.transform = `rotate(${clock}deg)`; // example of how to rotate the hand
+
+
